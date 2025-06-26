d5e312122977a96d72fcca23e8d5997cdb11af09
PsychoKillerdd <onlyvicenteuribe@gmail.com>
2025-06-26 14:20:29 -0400

    pnpm react

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+
+export default function Header() {
+  return (
+   <>
+   <header className="bg-slate-800">
+    <div className="mx-auto container px-5 py-16">
+        <div className="flex justify-between items-center">
+            <div className="">
+                <img className="w-32" src="/logo.svg" alt="Logotipo" />
+                {/* logo */}
+            </div>
+            <nav>
+
+            </nav>
+        </div>
+    </div>
+   </header>
+   </>
+  )
+}
