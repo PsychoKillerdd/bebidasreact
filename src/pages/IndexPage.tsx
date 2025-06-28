cb01e4f84116f5e970677645b5532133ca561531
PsychoKillerdd <onlyvicenteuribe@gmail.com>
2025-06-27 23:59:18 -0400

    ReactFirstCommit

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
-import Header from '../components/Header';
+
 
 export default function IndexPage() {
   return (
         <>
-        <Header />
-    
+          <h1>Index</h1>    
         </>  
 )}
