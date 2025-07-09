762dbc373b933fd8adc3b846b318ad6247068db8
Bocchi The Code (Vicente Uribe Galaz) <129336705+PsychoKillerdd@users.noreply.github.com>
2025-07-08 23:02:42 -0400

    redux cambio

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -4,6 +4,5 @@ import { createRecipesSlice, type RecipesSliceType } from './recipeSlice';
 
 export const useAppStore = create<RecipesSliceType>((...a) => ({
     ...createRecipesSlice(...a),
-    //hola
 
 }));
